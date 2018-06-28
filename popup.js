93b00f54d2d3302644beb3150f4871f1d1cde2c9
Kiichi Takeuchi <kiichi.takeuchi@iprocedures.com>
2018-06-28 02:18:44 -0400

    rc

---
+++ popup.js
@@ -0,0 +1,33 @@
+'use strict';
+//var queue = []; // use storage?
+function onClick(e) {
+	//var tabs = [{title:"hello " + new Date(),url:"http://google.com"}];
+	var dataType = $(this).attr('data-type');
+	var dataAction = $(this).attr('data-action');
+	chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+		var txt = 'ERROR - Sorry something went wrong.';
+		if (dataType == 'dash') {
+			txt = tabs[0].title+ ' - ' + tabs[0].url;
+		}
+		else if (dataType == 'bracket') {
+			txt = '[' + tabs[0].title+ '] ' + tabs[0].url;
+		}
+		else if (dataType == 'html') {
+			txt = '<a href="' + tabs[0].url+ '">' + tabs[0].url + '</a>';
+		}
+		else if (dataType == 'md') {
+			txt = '[' + tabs[0].title+ '](' + tabs[0].url + ')';
+		}
+
+		$('#clipboard').text(txt);
+		$('#clipboard').select();
+		document.execCommand('copy');
+		window.close();
+	});
+	//chrome.tabs.create({"url": "http://google.com"});
+	//chrome.tabs.executeScript(null, {code:"document.body.style.backgroundColor='" + e.target.id + "'"});
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  $('.menu-item').on('click', onClick);
+});
