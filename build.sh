d12fd9f2f16ae3908a826b4903215ad53b61adf5
Kiichi Takeuchi <kiichi.takeuchi@iprocedures.com>
2019-06-12 12:55:04 -0400

    version bump 1.3.0 and changed shell script

--- build.sh
+++ build.sh
@@ -2,5 +2,8 @@ rm src.zip
 zip -r src.zip src
 echo "Done. go to chrome developer site"
 ls -ltr src.zip
+echo ""
+echo "Did you bump the version in src/manifest ? "
+echo "Done. Go to the URL below"
 echo "https://chrome.google.com/webstore/developer/dashboard"
 open .
