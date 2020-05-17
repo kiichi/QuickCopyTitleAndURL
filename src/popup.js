fc0394f8b0b3d6c0b2300a590d92cf97bd8594f2
Kiichi Takeuchi <kiichi.takeuchi@iprocedures.com>
2020-05-17 11:11:20 -0400

    Design updates: bigger font and all tabs on left side

--- src/popup.js
+++ src/popup.js
@@ -13,13 +13,15 @@ function onClick(e) {
 function renderMenu(dataTypes, selectedDataType){
 	return dataTypes.map((item)=>{
 		return `<div class="pure-g">
-					<div class="pure-u-17-24">
+					<div class="pure-u-8-24">
+						<button class="pure-button text-center" 
+								data-type="${item.dataType}" 
+								data-action="all">All Tabs </button>
+					</div>
+					<div class="pure-u-16-24">
 						<button class="pure-button text-left" 
 								data-type="${item.dataType}" 
-								data-action="single">Copy ${item.description} ${selectedDataType === item.dataType ? '&#10003;':'&nbsp;'}</button>
-					</div>
-					<div class="pure-u-7-24">
-						<button class="pure-button" data-type="${item.dataType}" data-action="all">All Tabs</button>
+								data-action="single">Copy ${item.description} <span class="check">${selectedDataType === item.dataType ? '&#10003;':'&nbsp;'}</span></button>
 					</div>
 				</div>
 				`;
@@ -31,7 +33,7 @@ function renderMenu(dataTypes, selectedDataType){
 document.addEventListener('DOMContentLoaded', ()=>{
 	chrome.storage.local.get(['dataType'], (result)=>{
         let lastUsedDataType = result.dataType || 'dash';
-		$('.container').html(renderMenu(Lookup.DataTypes, lastUsedDataType));
+		$('#menu').html(renderMenu(Lookup.DataTypes, lastUsedDataType));
 		$('.pure-button').on('click', onClick);
     });
 });
